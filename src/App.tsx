b41926466a1960d0f9bc9fa38d5fced1a6a8a880
Sergey Sokolov <emaksae@yandex.ru>
2025-11-19 04:28:31 +0300

    1.00

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/storeyes">
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Navigate to="products" replace />} />
